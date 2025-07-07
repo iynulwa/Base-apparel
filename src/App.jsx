2cad94bfd422e23ea1bb7b763779303afed71ea0
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-07-07 20:32:28 +0100

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ function App() {
         <div className='container'>
           <h1><span className='color-h1'>WE'RE<br/></span>COMING <br/>SOON</h1>
           <p>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
-
           <Input />
         </div>
       </main>
